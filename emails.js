d41efaaff15f2a39f8fbd5562bfc3e772061caba
PRATEEKKUMARSHUKLA <109345218+PRATEEKKUMARSHUKLA@users.noreply.github.com>
2024-01-28 12:06:23 +0530

    Update and rename friendsList.js to emails.js

---
+++ emails.js
@@ -0,0 +1,16 @@
+// emails.js
+
+// Function to retrieve the list of email addresses
+function getEmailList() {
+    // Replace these email addresses with your actual email addresses
+    const emailList = [
+      'prateekkumarshukla10@gmail.com',
+        'prateekkumar5348@gmail.com',
+        // Add more email addresses as needed
+    ];
+
+    return emailList;
+}
+
+// Export the function to make it accessible from other files
+export { getEmailList };
