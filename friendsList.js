9c679837943a6a9c4116bef1e27e4cf10a94b1ea
PRATEEKKUMARSHUKLA <109345218+PRATEEKKUMARSHUKLA@users.noreply.github.com>
2024-01-28 11:48:54 +0530

    Update and rename script.js to friendsList.js

---
+++ friendsList.js
@@ -0,0 +1,10 @@
+// friendsList.js
+
+const friendsEmails = [
+  'prateekkumarshukla10@gmail.com',
+  'prateekkumar5348@gmail.com',
+  // Add more email addresses as needed
+];
+
+// Export the array for use in other files
+export { friendsEmails };
